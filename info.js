cd2e3b432380bd3fd28f4392f3ce728fab723bfa
madortill <mador.till@gmail.com>
2022-11-17 11:20:30 +0200

    Add files via upload

---
+++ info.js
@@ -0,0 +1,76 @@
+// // מערך המשפטים
+// const SENTENCES_TO_SORT = [
+//     {
+//         sen: "קבוצה 1", // המשפט
+//         group: "top" // לאיזה קבוצה הוא משתייך :top/bot
+//     },
+//     {
+//         sen: "קבוצה 2",
+//         group: "bot"
+//     },
+//     {
+//         sen: "קבוצה 1",
+//         group: "top"
+//     },
+//     {
+//         sen: "קבוצה 2",
+//         group: "bot"
+//     },
+//     {
+//         sen: "קבוצה 1",
+//         group: "top"
+//     },
+//     {
+//         sen: "קבוצה 2",
+//         group: "bot"
+//     },
+//     {
+//         sen: "קבוצה 1",
+//         group: "top"
+//     },
+//     {
+//         sen: "קבוצה 2",
+//         group: "bot"
+//     },
+    
+// ];
+
+const SENTENCES_TO_SORT = {
+    sortToGroups: {
+        drag: [
+            {
+                drag: "קבוצה 1",
+                group: 0
+            },
+            {
+                drag: "קבוצה 1",
+                group: 0
+            },
+            {
+                drag: "קבוצה 2",
+                group: 1
+            },
+            {
+                drag: "קבוצה 1",
+                group: 0
+            },
+            {
+                drag: "קבוצה 2",
+                group: 1
+            },
+            {
+                drag: "קבוצה 2",
+                group: 1
+            },
+            {
+                drag: "קבוצה 1",
+                group: 0
+            },
+            {
+                drag: "קבוצה 2",
+                group:  1
+            },
+        ],
+        grop: ["קבוצה 1", "קבוצה 2"]
+    }   
+};
